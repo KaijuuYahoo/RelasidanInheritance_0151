e108bccd3a275a6144820dbeda69ce410271f2fa
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-05-27 22:34:43 +0700

    Menambahkan folder Komposisi berisi Library Jantung.h

---
+++ Komposisi/Jantung.h
@@ -0,0 +1,17 @@
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class jantung{
+    public:
+        jantung (){
+            cout<< "jantung dihidupkan\n" <<endl; 
+        }
+        ~jantung(){
+            cout<< "jantung dimatikan\n" <<endl;  
+        }
+};
+
+#endif
